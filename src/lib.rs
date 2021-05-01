4bdec2e2d1317b5b2ce2394930d117bcc4f1dc93
YUJI <mail@yuji.ne.jp>
2021-05-01 17:22:09 +0900

    Enrich the explanation of the README

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+//! # CSVX - API Documentation -
+//!
+//! CSVX is an extension of CSV format.
+//! this extension allows you to use CSV as a spreadsheet.
+//!
+//! ## Example
+//!
+//! ```
+//! extern crate csvx;
+//!
+//! use csvx::Table;
+//! use std::error::Error;
+//!
+//! fn main() -> Result<(), Box<dyn Error>> {
+//!     let raw_csv = "pi,3^5,\"ref(0,0)\",-(1/0)
+//! 12%5,\"pow(3,5)\",0/NaN,\"\"\"Apollo\"\"\"
+//! A1+A2,\"if(true , sqrt(25),round(if(false,1.1,2.5)))\",D2+1969,";
+//!     println!("RAW CSV DATA:\n{}\n", raw_csv);
+//!
+//!     let mut table = Table::new(raw_csv)?;
+//!     println!("CALCULATED TABLE:\n{}", table);
+//!
+//!     table.update(0, 0, "true")?;
+//!     println!("UPDATED TABLE:\n{}", table.export_calculated_table()?);
+//!
+//!     table.expand(true);
+//!     println!("RAW TABLE:\n{}", table.export_raw_table()?);
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+
 mod node;
 mod table;
 mod token;
