0942748151206552a1a7d9b244218ffe5c35c67d
YUJI <mail@yuji.ne.jp>
2021-06-11 19:14:45 +0900

    Upgrade parser

--- src/node/parse.rs
+++ src/node/parse.rs
@@ -62,266 +62,147 @@ pub fn parse(seq: &Vec<Token>) -> (Box<ThreadSafeNode>, Vec<(usize, usize)>) {
         }
     }
 
-    let logical_or = parse_for_binary_operator(seq, Token::LogicalOr);
-    if let Some((left, right)) = logical_or {
-        return LogicalOr::new(vec![left, right]);
+    if let Some(x) = parse_for_binary_operator(seq, &[(Token::LogicalOr, LogicalOr::new)]) {
+        x
+    } else if let Some(x) =
+        parse_for_binary_operator(seq, &[(Token::LogicalAnd, LogicalAnd::new)])
+    {
+        x
+    } else if let Some(x) = parse_for_binary_operator(seq, &[(Token::BitwiseOr, BitwiseOr::new)])
+    {
+        x
+    } else if let Some(x) = parse_for_binary_operator(seq, &[(Token::Xor, Xor::new)]) {
+        x
+    } else if let Some(x) =
+        parse_for_binary_operator(seq, &[(Token::BitwiseAnd, BitwiseAnd::new)])
+    {
+        x
+    } else if let Some(x) = parse_for_binary_operator(
+        seq,
+        &[(Token::Equal, Equal::new), (Token::NotEqual, NotEqual::new)],
+    ) {
+        x
+    } else if let Some(x) = parse_for_binary_operator(
+        seq,
+        &[
+            (Token::LessThan, LessThan::new),
+            (Token::LessThanEqual, LessThanEqual::new),
+            (Token::GreaterThan, GreaterThan::new),
+            (Token::GreaterThanEqual, GreaterThanEqual::new),
+        ],
+    ) {
+        x
+    } else if let Some(x) = parse_for_binary_operator(
+        seq,
+        &[
+            (Token::LeftShift, LeftShift::new),
+            (Token::RightShift, RightShift::new),
+        ],
+    ) {
+        x
+    } else if let Some(x) =
+        parse_for_binary_operator(seq, &[(Token::Add, Add::new), (Token::Sub, Sub::new)])
+    {
+        x
+    } else if let Some(x) = parse_for_binary_operator(
+        seq,
+        &[
+            (Token::Mul, Mul::new),
+            (Token::Div, Div::new),
+            (Token::Mod, Mod::new),
+        ],
+    ) {
+        x
+    } else if let Some(x) =
+        parse_for_unary_operator(seq, &[(Token::Sub, Minus::new), (Token::Not, Not::new)])
+    {
+        x
+    } else if let Some(x) = parse_for_function(
+        seq,
+        &[
+            (Token::FnRef, FnRef::new, 2),
+            (Token::FnIf, FnIf::new, 3),
+            (Token::FnRound, FnRound::new, 1),
+            (Token::FnFloor, FnFloor::new, 1),
+            (Token::FnCeil, FnCeil::new, 1),
+            (Token::FnLog, FnLog::new, 2),
+            (Token::FnLn, FnLn::new, 1),
+            (Token::FnLog2, FnLog2::new, 1),
+            (Token::FnLog10, FnLog10::new, 1),
+            (Token::FnSqrt, FnSqrt::new, 1),
+            (Token::FnPow, FnPow::new, 2),
+            (Token::FnSin, FnSin::new, 1),
+            (Token::FnCos, FnCos::new, 1),
+            (Token::FnTan, FnTan::new, 1),
+            (Token::FnAsin, FnAsin::new, 1),
+            (Token::FnAcos, FnAcos::new, 1),
+            (Token::FnAtan, FnAtan::new, 1),
+            (Token::FnSinh, FnSinh::new, 1),
+            (Token::FnCosh, FnCosh::new, 1),
+            (Token::FnTanh, FnTanh::new, 1),
+            (Token::FnAsinh, FnAsinh::new, 1),
+            (Token::FnAcosh, FnAcosh::new, 1),
+            (Token::FnAtanh, FnAtanh::new, 1),
+        ],
+    ) {
+        x
+    } else if seq.len() == 1 {
+        Value::new(vec![vec![seq[0].clone()]])
+    } else {
+        (Box::new(Value::Error), vec![])
     }
-
-    let logical_and = parse_for_binary_operator(seq, Token::LogicalAnd);
-    if let Some((left, right)) = logical_and {
-        return LogicalAnd::new(vec![left, right]);
-    }
-
-    let bitwise_or = parse_for_binary_operator(seq, Token::BitwiseOr);
-    if let Some((left, right)) = bitwise_or {
-        return BitwiseOr::new(vec![left, right]);
-    }
-
-    let bitwise_and = parse_for_binary_operator(seq, Token::BitwiseAnd);
-    if let Some((left, right)) = bitwise_and {
-        return BitwiseAnd::new(vec![left, right]);
-    }
-
-    let equal = parse_for_binary_operator(seq, Token::Equal);
-    if let Some((left, right)) = equal {
-        return Equal::new(vec![left, right]);
-    }
-
-    let not_equal = parse_for_binary_operator(seq, Token::NotEqual);
-    if let Some((left, right)) = not_equal {
-        return NotEqual::new(vec![left, right]);
-    }
-
-    let less_than = parse_for_binary_operator(seq, Token::LessThan);
-    if let Some((left, right)) = less_than {
-        return LessThan::new(vec![left, right]);
-    }
-
-    let less_than_equal = parse_for_binary_operator(seq, Token::LessThanEqual);
-    if let Some((left, right)) = less_than_equal {
-        return LessThanEqual::new(vec![left, right]);
-    }
-
-    let greater_than = parse_for_binary_operator(seq, Token::GreaterThan);
-    if let Some((left, right)) = greater_than {
-        return GreaterThan::new(vec![left, right]);
-    }
-
-    let greater_than_equal = parse_for_binary_operator(seq, Token::GreaterThanEqual);
-    if let Some((left, right)) = greater_than_equal {
-        return GreaterThanEqual::new(vec![left, right]);
-    }
-
-    let left_shift = parse_for_binary_operator(seq, Token::LeftShift);
-    if let Some((left, right)) = left_shift {
-        return LeftShift::new(vec![left, right]);
-    }
-
-    let right_shift = parse_for_binary_operator(seq, Token::RightShift);
-    if let Some((left, right)) = right_shift {
-        return RightShift::new(vec![left, right]);
-    }
-
-    let add = parse_for_binary_operator(seq, Token::Add);
-    if let Some((left, right)) = add {
-        return Add::new(vec![left, right]);
-    }
-
-    let sub = parse_for_binary_operator(seq, Token::Sub);
-    if let Some((left, right)) = sub {
-        return Sub::new(vec![left, right]);
-    }
-
-    let mul = parse_for_binary_operator(seq, Token::Mul);
-    if let Some((left, right)) = mul {
-        return Mul::new(vec![left, right]);
-    }
-
-    let div = parse_for_binary_operator(seq, Token::Div);
-    if let Some((left, right)) = div {
-        return Div::new(vec![left, right]);
-    }
-
-    let modulo = parse_for_binary_operator(seq, Token::Mod);
-    if let Some((left, right)) = modulo {
-        return Mod::new(vec![left, right]);
-    }
-
-    let xor = parse_for_binary_operator(seq, Token::Xor);
-    if let Some((left, right)) = xor {
-        return Xor::new(vec![left, right]);
-    }
-
-    let minus = parse_for_unary_operator(seq, Token::Sub);
-    if let Some(leaf) = minus {
-        return Minus::new(vec![leaf]);
-    }
-
-    let not = parse_for_unary_operator(seq, Token::Not);
-    if let Some(leaf) = not {
-        return Not::new(vec![leaf]);
-    }
-
-    let fn_ref = parse_for_function(seq, Token::FnRef, 2);
-    if let Some(args) = fn_ref {
-        return FnRef::new(args);
-    }
-
-    let fn_if = parse_for_function(seq, Token::FnIf, 3);
-    if let Some(args) = fn_if {
-        return FnIf::new(args);
-    }
-
-    let fn_round = parse_for_function(seq, Token::FnRound, 1);
-    if let Some(args) = fn_round {
-        return FnRound::new(args);
-    }
-
-    let fn_floor = parse_for_function(seq, Token::FnFloor, 1);
-    if let Some(args) = fn_floor {
-        return FnFloor::new(args);
-    }
-
-    let fn_ceil = parse_for_function(seq, Token::FnCeil, 1);
-    if let Some(args) = fn_ceil {
-        return FnCeil::new(args);
-    }
-
-    let fn_log = parse_for_function(seq, Token::FnLog, 2);
-    if let Some(args) = fn_log {
-        return FnLog::new(args);
-    }
-
-    let fn_ln = parse_for_function(seq, Token::FnLn, 1);
-    if let Some(args) = fn_ln {
-        return FnLn::new(args);
-    }
-
-    let fn_log2 = parse_for_function(seq, Token::FnLog2, 1);
-    if let Some(args) = fn_log2 {
-        return FnLog2::new(args);
-    }
-
-    let fn_log10 = parse_for_function(seq, Token::FnLog10, 1);
-    if let Some(args) = fn_log10 {
-        return FnLog10::new(args);
-    }
-
-    let fn_sqrt = parse_for_function(seq, Token::FnSqrt, 1);
-    if let Some(args) = fn_sqrt {
-        return FnSqrt::new(args);
-    }
-
-    let fn_pow = parse_for_function(seq, Token::FnPow, 2);
-    if let Some(args) = fn_pow {
-        return FnPow::new(args);
-    }
-
-    let fn_sin = parse_for_function(seq, Token::FnSin, 1);
-    if let Some(args) = fn_sin {
-        return FnSin::new(args);
-    }
-
-    let fn_cos = parse_for_function(seq, Token::FnCos, 1);
-    if let Some(args) = fn_cos {
-        return FnCos::new(args);
-    }
-
-    let fn_tan = parse_for_function(seq, Token::FnTan, 1);
-    if let Some(args) = fn_tan {
-        return FnTan::new(args);
-    }
-
-    let fn_asin = parse_for_function(seq, Token::FnAsin, 1);
-    if let Some(args) = fn_asin {
-        return FnAsin::new(args);
-    }
-
-    let fn_acos = parse_for_function(seq, Token::FnAcos, 1);
-    if let Some(args) = fn_acos {
-        return FnAcos::new(args);
-    }
-
-    let fn_atan = parse_for_function(seq, Token::FnAtan, 1);
-    if let Some(args) = fn_atan {
-        return FnAtan::new(args);
-    }
-
-    let fn_sinh = parse_for_function(seq, Token::FnSinh, 1);
-    if let Some(args) = fn_sinh {
-        return FnSinh::new(args);
-    }
-
-    let fn_cosh = parse_for_function(seq, Token::FnCosh, 1);
-    if let Some(args) = fn_cosh {
-        return FnCosh::new(args);
-    }
-
-    let fn_tanh = parse_for_function(seq, Token::FnTanh, 1);
-    if let Some(args) = fn_tanh {
-        return FnTanh::new(args);
-    }
-
-    let fn_asinh = parse_for_function(seq, Token::FnAsinh, 1);
-    if let Some(args) = fn_asinh {
-        return FnAsinh::new(args);
-    }
-
-    let fn_acosh = parse_for_function(seq, Token::FnAcosh, 1);
-    if let Some(args) = fn_acosh {
-        return FnAcosh::new(args);
-    }
-
-    let fn_atanh = parse_for_function(seq, Token::FnAtanh, 1);
-    if let Some(args) = fn_atanh {
-        return FnAtanh::new(args);
-    }
-
-    if seq.len() == 1 {
-        return Value::new(vec![vec![seq[0].clone()]]);
-    }
-
-    (Box::new(Value::Error), vec![])
 }
 
 fn parse_for_binary_operator(
     seq: &Vec<Token>,
-    op_target: Token,
-) -> Option<(Vec<Token>, Vec<Token>)> {
+    op_targets: &[(
+        Token,
+        fn(seqs: Vec<Vec<Token>>) -> (Box<ThreadSafeNode>, Vec<(usize, usize)>),
+    )],
+) -> Option<(Box<ThreadSafeNode>, Vec<(usize, usize)>)> {
     let mut seq_before_op = vec![];
     let mut seq_after_op = vec![];
-    let (is_op_found, _) = seq
-        .iter()
-        .fold((false, 0), |(is_already_found, depth), token| {
-            let depth_next = match token {
-                Token::ParenthesisBegin => depth + 1,
-                Token::ParenthesisEnd => depth - 1,
-                _ => depth,
-            };
+    let (initializer, _) = seq.iter().fold((None, 0), |(initializer, depth), token| {
+        let depth_next = match token {
+            Token::ParenthesisBegin => depth + 1,
+            Token::ParenthesisEnd => depth - 1,
+            _ => depth,
+        };
 
-            if is_already_found {
-                seq_after_op.push(token.clone());
-                (true, depth_next)
-            } else {
-                if token == &op_target && depth == 0 && seq_before_op.len() != 0 {
-                    (true, 0)
-                } else {
-                    seq_before_op.push(token.clone());
-                    (false, depth_next)
-                }
-            }
-        });
+        if let Some(_) = initializer {
+            seq_after_op.push(token.clone());
+            (initializer, depth_next)
+        } else if let (Some((_, initializer)), true, true) = (
+            op_targets.iter().find(|(op_target, _)| op_target == token),
+            depth == 0,
+            seq_before_op.len() != 0,
+        ) {
+            (Some(initializer), 0)
+        } else {
+            seq_before_op.push(token.clone());
+            (None, depth_next)
+        }
+    });
 
-    if is_op_found == true && seq_after_op.len() != 0 {
-        Some((seq_before_op, seq_after_op))
+    if let (Some(initializer), true) = (initializer, seq_after_op.len() != 0) {
+        Some(initializer(vec![seq_before_op, seq_after_op]))
     } else {
         None
     }
 }
 
-fn parse_for_unary_operator(seq: &Vec<Token>, op_target: Token) -> Option<Vec<Token>> {
-    if seq.first() == Some(&op_target) {
-        Some(seq[1..].to_vec())
+fn parse_for_unary_operator(
+    seq: &Vec<Token>,
+    op_targets: &[(
+        Token,
+        fn(seqs: Vec<Vec<Token>>) -> (Box<ThreadSafeNode>, Vec<(usize, usize)>),
+    )],
+) -> Option<(Box<ThreadSafeNode>, Vec<(usize, usize)>)> {
+    if let Some((_, initializer)) = op_targets
+        .iter()
+        .find(|(op_target, _)| Some(op_target) == seq.first())
+    {
+        Some(initializer(vec![seq[1..].to_vec()]))
     } else {
         None
     }
@@ -329,14 +210,23 @@ fn parse_for_unary_operator(seq: &Vec<Token>, op_target: Token) -> Option<Vec<To
 
 fn parse_for_function(
     seq: &Vec<Token>,
-    op_target: Token,
-    num_of_args: usize,
-) -> Option<Vec<Vec<Token>>> {
-    if seq.len() > 3
-        && seq.first() == Some(&op_target)
-        && seq[1] == Token::ParenthesisBegin
-        && seq.last() == Some(&Token::ParenthesisEnd)
-    {
+    op_targets: &[(
+        Token,
+        fn(seqs: Vec<Vec<Token>>) -> (Box<ThreadSafeNode>, Vec<(usize, usize)>),
+        usize,
+    )],
+) -> Option<(Box<ThreadSafeNode>, Vec<(usize, usize)>)> {
+    if let (
+        Some((_, initializer, num_of_args)),
+        Some(&Token::ParenthesisBegin),
+        Some(&Token::ParenthesisEnd),
+    ) = (
+        op_targets
+            .iter()
+            .find(|(op_target, _, _)| Some(op_target) == seq.first()),
+        seq.get(1),
+        seq.last(),
+    ) {
         let mut args = vec![];
         let mut arg = vec![];
         let mut depth = 0;
@@ -356,9 +246,12 @@ fn parse_for_function(
         }
         args.push(arg);
 
-        if args.len() == num_of_args {
-            return Some(args);
+        if &args.len() == num_of_args {
+            Some(initializer(args))
+        } else {
+            None
         }
+    } else {
+        None
     }
-    None
 }
